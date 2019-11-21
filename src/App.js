86b4374fc940219f0057a16de9829a02a5a80086
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-11-21 15:09:54 +0000

    added display function

--- src/App.js
+++ src/App.js
@@ -3,45 +3,43 @@ import './App.css';
 
 class App extends React.Component {
   state = {
-    display: "",
-    n1: "",
-    n2: "",
+    display: 0,
+    n1: 0,
+    n2: 0,
     op: "",
     numberEntry: 1,
-    result: ""  
+    result: 0
   }
 
   numberButton = (n) => {
     if (this.state.numberEntry === 1) {
-      this.setState({n1: this.state.n1 + n });
+      this.setState({ n1: this.state.n1 + n });
+      console.log("number inputted");
       console.log(this.state.n1);
     }
     if (this.state.numberEntry === 2) {
-      this.setState({n2: this.state.n2 + n });
+      this.setState({ n2: this.state.n2 + n });
+      console.log("number inputted");
       console.log(this.state.n2);
     }
   }
 
   calculate = () => {
     if (this.state.op === "+") {
-      this.setState({result: this.state.n1 + this.state.n2 });
-      console.log(this.state.result);
-      return this.state.result
+      this.setState({ result: this.state.n1 + this.state.n2 });
+      
     }
     if (this.state.op === "x") {
-      this.setState({result: this.state.n1 * this.state.n2 }); 
+      this.setState({ result: this.state.n1 * this.state.n2 });
       console.log(this.state.result);
-      return this.state.result
     }
     if (this.state.op === "-") {
-      this.setState({result: this.state.n1 - this.state.n2 });
+      this.setState({ result: this.state.n1 - this.state.n2 });
       console.log(this.state.result);
-      return this.state.result
     }
     if (this.state.op === "÷") {
-      this.setState({result: this.state.n1 / this.state.n2 });
+      this.setState({ result: this.state.n1 / this.state.n2 });
       console.log(this.state.result);
-      return this.state.result
     }
 
   }
@@ -49,34 +47,40 @@ class App extends React.Component {
   operatorChoice = (operator) => {
     this.setState({
       op: operator,
-      numberEntry: 2
+      numberEntry: 2,
     })
-
-
+    console.log(this.state.numberEntry);
+    console.log(this.state.op)
   }
 
   clear = () => {
     this.setState({
-      display: "",
-      n1: "",
-      n2: "",
-      op: "",
+      display: 0,
+      n1: 0,
+      n2: 0,
+      op: 0,
       numberEntry: 1,
-      result: ""
+      result: 0
     })
+  }
+
+  updateDisplay = () => {
+    
   }
 
 
   render() {
+
+    console.log(this.state.result);
     return (
       <div className="calc">
-        <input className="calcDisplay" readOnly="readonly" ></input>
+        <p className="calcDisplay">{this.state.display}</p>
         <button className="clear" onClick={() => this.clear()} >Clear</button>
         <button className="divide" onClick={() => this.operatorChoice("÷")} >÷</button>
         <button className="seven" onClick={() => this.numberButton(7)} >7</button>
         <button className="eight" onClick={() => this.numberButton(8)} >8</button>
         <button className="nine" onClick={() => this.numberButton(9)} >9</button>
-        <button className="subtract" onClick={() => this.operatorChoice("-")} >-</button>
+        <button className="subtract" onClick={() => this.operatorChoice('-')} >-</button>
         <button className="four" onClick={() => this.numberButton(4)} >4</button>
         <button className="five" onClick={() => this.numberButton(5)} >5</button>
         <button className="six" onClick={() => this.numberButton(6)} >6</button>
